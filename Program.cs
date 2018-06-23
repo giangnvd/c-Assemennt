6f30c4e73260d1346637bb1da284fdfa3d8e4602
van_giang <giangnvd00570@fpt.edu.vn>
2018-06-23 22:24:07 +0700

    nop bai

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using YangYesterday.entity;
+using YangYesterday.view;
+
+namespace YangYesterday
+{
+    class Program
+    {
+        public static YYAccount currentLoggedInYyAccount;
+
+        static void Main(string[] args)
+        {
+            ApplicationView view = new ApplicationView();
+            while (true)
+            {
+                if (Program.currentLoggedInYyAccount != null)
+                {
+                    view.GenerateCustomerMenu();
+                }
+                else
+                {
+                    view.GenerateDefaultMenu();
+                }
+            }
+        }
+    }
+}
